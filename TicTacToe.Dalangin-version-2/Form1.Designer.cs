79c87945eea407832698b06cc9e8889e2db8f152
Kate Anne Marie Dalangin <104249599+kateannemarie@users.noreply.github.com>
2022-06-03 09:32:33 +0800

    New file due to encountered error

---
+++ TicTacToe.Dalangin-version-2/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace TicTacToe.Dalangin_version_2
+{
+    partial class TicTacToeLabel
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicTacToeLabel));
+            this.ButtonA1 = new System.Windows.Forms.Button();
+            this.ButtonA2 = new System.Windows.Forms.Button();
+            this.ButtonA3 = new System.Windows.Forms.Button();
+            this.ButtonB1 = new System.Windows.Forms.Button();
+            this.ButtonB2 = new System.Windows.Forms.Button();
+            this.ButtonB3 = new System.Windows.Forms.Button();
+            this.ButtonC1 = new System.Windows.Forms.Button();
+            this.ButtonC2 = new System.Windows.Forms.Button();
+            this.ButtonC3 = new System.Windows.Forms.Button();
+            this.playerInstructions = new System.Windows.Forms.Button();
+            this.newgameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonA1
+            // 
+            this.ButtonA1.Font = new System.Drawing.Font("Century Schoolbook", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonA1.Location = new System.Drawing.Point(27, 85);
+            this.ButtonA1.Name = "ButtonA1";
+            this.ButtonA1.Size = new System.Drawing.Size(85, 76);
+            this.ButtonA1.TabIndex = 0;
+            this.ButtonA1.UseVisualStyleBackColor = true;
+            this.ButtonA1.Click += new System.EventHandler(this.ButtonA1_Click);
+            // 
+            // ButtonA2
+            // 
+            this.ButtonA2.Font = new System.Drawing.Font("Century Schoolbook", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonA2.Location = new System.Drawing.Point(118, 85);
+            this.ButtonA2.Name = "ButtonA2";
+            this.ButtonA2.Size = new System.Drawing.Size(85, 76);
+            this.ButtonA2.TabIndex = 1;
+            this.ButtonA2.UseVisualStyleBackColor = true;
+            this.ButtonA2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ButtonA3
+            // 
+            this.ButtonA3.Font = new System.Drawing.Font("Century Schoolbook", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonA3.Location = new System.Drawing.Point(209, 85);
+            this.ButtonA3.Name = "ButtonA3";
+            this.ButtonA3.Size = new System.Drawing.Size(85, 76);
+            this.ButtonA3.TabIndex = 2;
+            this.ButtonA3.UseVisualStyleBackColor = true;
+            this.ButtonA3.Click += new System.EventHandler(this.ButtonA3_Click);
+            // 
+            // ButtonB1
+            // 
+            this.ButtonB1.Font = new System.Drawing.Font("Century Schoolbook", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonB1.Location = new System.Drawing.Point(27, 167);
+            this.ButtonB1.Name = "ButtonB1";
+            this.ButtonB1.Size = new System.Drawing.Size(85, 76);
+            this.ButtonB1.TabIndex = 3;
+            this.ButtonB1.UseVisualStyleBackColor = true;
+            this.ButtonB1.Click += new System.EventHandler(this.ButtonB1_Click);
+            // 
+            // ButtonB2
+            // 
+            this.ButtonB2.Font = new System.Drawing.Font("Century Schoolbook", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonB2.Location = new System.Drawing.Point(118, 167);
+            this.ButtonB2.Name = "ButtonB2";
+            this.ButtonB2.Size = new System.Drawing.Size(85, 76);
+            this.ButtonB2.TabIndex = 4;
+            this.ButtonB2.UseVisualStyleBackColor = true;
+            this.ButtonB2.Click += new System.EventHandler(this.ButtonB2_Click);
+            // 
+            // ButtonB3
+            // 
+            this.ButtonB3.Font = new System.Drawing.Font("Century Schoolbook", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonB3.Location = new System.Drawing.Point(209, 167);
+            this.ButtonB3.Name = "ButtonB3";
+            this.ButtonB3.Size = new System.Drawing.Size(85, 76);
+            this.ButtonB3.TabIndex = 5;
+            this.ButtonB3.UseVisualStyleBackColor = true;
+            this.ButtonB3.Click += new System.EventHandler(this.ButtonB3_Click);
+            // 
+            // ButtonC1
+            // 
+            this.ButtonC1.Font = new System.Drawing.Font("Century Schoolbook", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonC1.Location = new System.Drawing.Point(27, 249);
+            this.ButtonC1.Name = "ButtonC1";
+            this.ButtonC1.Size = new System.Drawing.Size(85, 76);
+            this.ButtonC1.TabIndex = 6;
+            this.ButtonC1.UseVisualStyleBackColor = true;
+            this.ButtonC1.Click += new System.EventHandler(this.ButtonC1_Click);
+            // 
+            // ButtonC2
+            // 
+            this.ButtonC2.Font = new System.Drawing.Font("Century Schoolbook", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonC2.Location = new System.Drawing.Point(118, 249);
+            this.ButtonC2.Name = "ButtonC2";
+            this.ButtonC2.Size = new System.Drawing.Size(85, 76);
+            this.ButtonC2.TabIndex = 7;
+            this.ButtonC2.UseVisualStyleBackColor = true;
+            this.ButtonC2.Click += new System.EventHandler(this.ButtonC2_Click);
+            // 
+            // ButtonC3
+            // 
+            this.ButtonC3.Font = new System.Drawing.Font("Century Schoolbook", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonC3.Location = new System.Drawing.Point(209, 249);
+            this.ButtonC3.Name = "ButtonC3";
+            this.ButtonC3.Size = new System.Drawing.Size(85, 76);
+            this.ButtonC3.TabIndex = 8;
+            this.ButtonC3.UseVisualStyleBackColor = true;
+            this.ButtonC3.Click += new System.EventHandler(this.ButtonC3_Click);
+            // 
+            // playerInstructions
+            // 
+            this.playerInstructions.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.playerInstructions.ForeColor = System.Drawing.Color.LightCoral;
+            this.playerInstructions.Location = new System.Drawing.Point(12, 32);
+            this.playerInstructions.Name = "playerInstructions";
+            this.playerInstructions.Size = new System.Drawing.Size(75, 23);
+            this.playerInstructions.TabIndex = 9;
+            this.playerInstructions.Text = "How to play?";
+            this.playerInstructions.UseVisualStyleBackColor = true;
+            // 
+            // newgameButton
+            // 
+            this.newgameButton.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.newgameButton.ForeColor = System.Drawing.Color.LightCoral;
+            this.newgameButton.Location = new System.Drawing.Point(234, 32);
+            this.newgameButton.Name = "newgameButton";
+            this.newgameButton.Size = new System.Drawing.Size(75, 23);
+            this.newgameButton.TabIndex = 10;
+            this.newgameButton.Text = "New Game";
+            this.newgameButton.UseVisualStyleBackColor = true;
+            // 
+            // TicTacToeLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 344);
+            this.Controls.Add(this.newgameButton);
+            this.Controls.Add(this.playerInstructions);
+            this.Controls.Add(this.ButtonC3);
+            this.Controls.Add(this.ButtonC2);
+            this.Controls.Add(this.ButtonC1);
+            this.Controls.Add(this.ButtonB3);
+            this.Controls.Add(this.ButtonB2);
+            this.Controls.Add(this.ButtonB1);
+            this.Controls.Add(this.ButtonA3);
+            this.Controls.Add(this.ButtonA2);
+            this.Controls.Add(this.ButtonA1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(337, 383);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(337, 383);
+            this.Name = "TicTacToeLabel";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button ButtonA1;
+        private Button ButtonA2;
+        private Button ButtonA3;
+        private Button ButtonB1;
+        private Button ButtonB2;
+        private Button ButtonB3;
+        private Button ButtonC1;
+        private Button ButtonC2;
+        private Button ButtonC3;
+        private Button playerInstructions;
+        private Button newgameButton;
+    }
+}
---
+++ TicTacToe.Dalangin-version-2/Form1.cs
@@ -0,0 +1,276 @@
+namespace TicTacToe.Dalangin_version_2
+{
+    public partial class TicTacToeLabel : Form
+    {
+        public TicTacToeLabel()
+        {
+            InitializeComponent();
+        }
+
+        String[] keyPad = new string[9];
+        int currentPlayer = 0;
+
+        public String backSymbol(int turn)
+        {
+            if (turn % 2 == 0)
+            {
+                return "O";
+            }
+            else
+            {
+                return "X";
+            }
+        }
+
+        public System.Drawing.Color colorChanger(String symbol)
+        {
+            if (symbol.Equals("O"))
+            {
+                return System.Drawing.Color.LightYellow;
+            }
+            else if (symbol.Equals("X"))
+            {
+                return System.Drawing.Color.LightBlue;
+            }
+            return System.Drawing.Color.Silver;
+        }
+
+        public void theWinner()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                String combination = "";
+                int one = 0, two = 0, three = 0;
+
+
+                switch (i)
+                {
+                    case 0:
+                        combination = keyPad[2] + keyPad[4] + keyPad[6];
+                        one = 2;
+                        two = 4;
+                        three = 6;
+                        break;
+                    case 1:
+                        combination = keyPad[0] + keyPad[4] + keyPad[8];
+                        one = 0;
+                        two = 4;
+                        three = 8;
+                        break;
+                    case 2:
+                        combination = keyPad[0] + keyPad[1] + keyPad[2];
+                        one = 0;
+                        two = 1;
+                        three = 2;
+                        break;
+                    case 3:
+                        combination = keyPad[3] + keyPad[4] + keyPad[5];
+                        one = 3;
+                        two = 4;
+                        three = 5;
+                        break;
+                    case 4:
+                        combination = keyPad[6] + keyPad[7] + keyPad[8];
+                        one = 6;
+                        two = 7;
+                        three = 8;
+                        break;
+                    case 5:
+                        combination = keyPad[2] + keyPad[5] + keyPad[8];
+                        one = 2;
+                        two = 5;
+                        three = 8;
+                        break;
+                    case 6:
+                        combination = keyPad[1] + keyPad[4] + keyPad[7];
+                        one = 1;
+                        two = 4;
+                        three = 7;
+                        break;
+                    case 7:
+                        combination = keyPad[0] + keyPad[3] + keyPad[6];
+                        one = 0;
+                        two = 3;
+                        three = 6;
+                        break;
+                }
+
+                if (combination.Equals("OOO"))
+                {
+                    winnerColor(one);
+                    winnerColor(two);
+                    winnerColor(three);
+                    MessageBox.Show("Player O has won the game!", "We have a winner!", MessageBoxButtons.OK);
+                }
+                else if (combination.Equals("XXX"))
+                {
+                    winnerColor(one);
+                    winnerColor(two);
+                    winnerColor(three);
+                    MessageBox.Show("Player X has won the game!", "We have a winner!", MessageBoxButtons.OK);
+                }
+
+                else tieBreaker();
+
+
+            }
+        }
+
+        public void reset()
+        {
+            ButtonA1.Text = "";
+            ButtonA2.Text = "";
+            ButtonA3.Text = "";
+            ButtonB1.Text = "";
+            ButtonB2.Text = "";
+            ButtonB3.Text = "";
+            ButtonC1.Text = "";
+            ButtonC2.Text = "";
+            ButtonC3.Text = "";
+            ButtonA1.BackColor = System.Drawing.Color.Silver;
+            ButtonA2.BackColor = System.Drawing.Color.Silver;
+            ButtonA3.BackColor = System.Drawing.Color.Silver;
+            ButtonB1.BackColor = System.Drawing.Color.Silver;
+            ButtonB2.BackColor = System.Drawing.Color.Silver;
+            ButtonB3.BackColor = System.Drawing.Color.Silver;
+            ButtonC1.BackColor = System.Drawing.Color.Silver;
+            ButtonC2.BackColor = System.Drawing.Color.Silver;
+            ButtonC3.BackColor = System.Drawing.Color.Silver;
+            keyPad = new string[9];
+            currentPlayer = 0;
+        }
+
+        public void winnerColor(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    ButtonA1.BackColor = System.Drawing.Color.Green; break;
+                case 1:
+                    ButtonA2.BackColor = System.Drawing.Color.Green; break;
+                case 2:
+                    ButtonA3.BackColor = System.Drawing.Color.Green; break;
+                case 3:
+                    ButtonB1.BackColor = System.Drawing.Color.Green; break;
+                case 4:
+                    ButtonB2.BackColor = System.Drawing.Color.Green; break;
+                case 5:
+                    ButtonB3.BackColor = System.Drawing.Color.Green; break;
+                case 6:
+                    ButtonC1.BackColor = System.Drawing.Color.Green; break;
+                case 7:
+                    ButtonC2.BackColor = System.Drawing.Color.Green; break;
+                case 8:
+                    ButtonC3.BackColor = System.Drawing.Color.Green; break;
+
+            }
+        }
+
+        public void tieBreaker()
+        {
+            int counter = 0;
+            for (int i = 0; i < keyPad.Length; i++)
+            {
+                if (keyPad[i] != null) { counter++; }
+
+                if (counter == 9)
+                {
+                    reset();
+                    MessageBox.Show("That's a draw!", "No winner", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentPlayer++;
+            keyPad[1] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[1]);
+            ButtonA2.BackColor = keypadColor;
+            ButtonA2.Text = keyPad[1];
+            theWinner();
+        }
+
+        private void ButtonA1_Click(object sender, EventArgs e)
+        {
+            currentPlayer++;
+            keyPad[0] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[0]);
+            ButtonA1.BackColor = keypadColor;
+            ButtonA1.Text = keyPad[0];
+            theWinner();
+        }
+
+        private void ButtonA3_Click(object sender, EventArgs e)
+        {
+            currentPlayer++;
+            keyPad[2] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[2]);
+            ButtonA3.BackColor = keypadColor;
+            ButtonA3.Text = keyPad[2];
+            theWinner();
+        }
+
+        private void ButtonB1_Click(object sender, EventArgs e)
+        {
+            currentPlayer++;
+            keyPad[3] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[3]);
+            ButtonB1.BackColor = keypadColor;
+            ButtonB1.Text = keyPad[3];
+            theWinner();
+        }
+
+        private void ButtonB2_Click(object sender, EventArgs e)
+        {
+            currentPlayer++;
+            keyPad[4] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[4]);
+            ButtonB2.BackColor = keypadColor;
+            ButtonB2.Text = keyPad[4];
+            theWinner();
+        }
+
+        private void ButtonB3_Click(object sender, EventArgs e)
+        {
+            currentPlayer++;
+            keyPad[5] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[5]);
+            ButtonB3.BackColor = keypadColor;
+            ButtonB3.Text = keyPad[5];
+            theWinner();
+        }
+
+        private void ButtonC1_Click(object sender, EventArgs e)
+        {
+            currentPlayer++;
+            keyPad[6] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[6]);
+            ButtonC1.BackColor = keypadColor;
+            ButtonC1.Text = keyPad[6];
+            theWinner();
+        }
+
+        private void ButtonC2_Click(object sender, EventArgs e)
+        {
+            currentPlayer++;
+            keyPad[7] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[7]);
+            ButtonC2.BackColor = keypadColor;
+            ButtonC2.Text = keyPad[7];
+            theWinner();
+        }
+
+        private void ButtonC3_Click(object sender, EventArgs e)
+        {
+            currentPlayer++;
+            keyPad[8] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[8]);
+            ButtonC3.BackColor = keypadColor;
+            ButtonC3.Text = keyPad[8];
+            theWinner();
+        }
+    }
+}
